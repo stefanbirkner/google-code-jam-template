ed71ce96966443d1483194c729f46d5899333ea6
Stefan Birkner <mail@stefan-birkner.de>
2014-04-12 02:14:58 +0200

    Initial version.

---
+++ Problem.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Problem {
+	public static void main(String... args) {
+		for (DataSet dataSet : new DataSets())
+			handleDataSet(dataSet);
+	}
+
+
+	private static void handleDataSet(DataSet dataSet) {
+		System.out.println(
+			"Case #" + dataSet.number + ": " + dataSet.getResult());
+	}
+
+	public static class DataSet {
+		public long number;
+
+		public String getResult() {
+			//calculate result
+			return "your result";		
+		}
+	}
+
+	public static class DataSets implements Iterable<DataSet> {
+		public Iterator<DataSet> iterator() {
+			return new DataSetsIterator();
+		}		
+	}
+
+	public static class DataSetsIterator implements Iterator<DataSet> {
+		private final long numberOfDataSets;
+		private final Scanner sc;
+		private long indexOfCurrentDataSet; //zero based
+
+		public DataSetsIterator() {
+			sc = new Scanner(System.in);
+			numberOfDataSets = sc.nextLong();
+			nextLine();
+			indexOfCurrentDataSet = 0;
+		}
+
+		public boolean hasNext() {
+			return indexOfCurrentDataSet < numberOfDataSets;
+		}
+
+		public DataSet next() {
+			DataSet dataSet = new DataSet();
+			dataSet.number = ++indexOfCurrentDataSet;
+			return dataSet;
+		}
+
+		private void nextLine() {
+			sc.nextLine();
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Java Template for Google Code Jam
+=================================
+
+1. Add fields to `Problem.DataSet`.
+2. Implement `Problem.DataSetsIterator.next()`. It has to set the fields of the data set.
+3. Implement `Problem.DataSet.getResult()`. This is the real work.
+4. Compile: `javac Problem.java`
+5. Run it: `cat input | java Problem`
